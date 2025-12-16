1acb2621cd3e3f6742d3b0ade51f452a2188fcce
Juno Lee <juno@junolee.com>
2025-12-16 04:23:09 +0000

    update incremental update logic in date_dim

--- dbt/models/walmart/silver/walmart_date_dim.sql
+++ dbt/models/walmart/silver/walmart_date_dim.sql
@@ -11,20 +11,25 @@ WITH dates AS ( {{
         datepart="week",
         start_date="cast('2010-02-05' as date)",
         end_date="cast('2013-07-26' as date)") }}
-), stg_fact AS (
+), 
+
+stg_fact AS (
     SELECT
         store_date,
         isholiday,
-        MAX(loaded_at) AS loaded_at
+        loaded_at
     FROM {{ ref("stg_fact_raw") }}
     {% if is_incremental() %}
-    WHERE loaded_at > (SELECT COALESCE(MAX(loaded_at), '1900-01-01') FROM {{ this }})
+    WHERE loaded_at > (SELECT COALESCE(MAX(loaded_at), '1900-01-01'::timestamp) FROM {{this}})
     {% endif %}
-    GROUP BY (store_date, isholiday)
-), existing AS (
-    SELECT date_id, store_date, isholiday, insert_date, update_date
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY store_date ORDER BY loaded_at DESC) = 1
+),
+
+existing AS (
+    SELECT store_date, insert_date
     FROM {{ this }}
 )
+
 SELECT
     TO_VARCHAR(d.date_week, 'yyyyMMdd')::INT AS date_id,
     d.date_week AS store_date,
@@ -33,6 +38,6 @@ SELECT
     CURRENT_TIMESTAMP() AS update_date,
     loaded_at    
 FROM dates AS d
-LEFT OUTER JOIN stg_fact AS f ON d.date_week = f.store_date
+LEFT JOIN stg_fact AS f ON d.date_week = f.store_date
 LEFT JOIN existing e ON d.date_week = e.store_date
 
