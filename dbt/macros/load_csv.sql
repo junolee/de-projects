ff807b29cbcbb21dc1ca44cbb7a267f649e1de8d
Juno Lee <juno@junolee.com>
2025-12-17 16:02:49 +0000

    update bronze config and macro

--- dbt/macros/load_csv.sql
+++ dbt/macros/load_csv.sql
@@ -1,11 +1,14 @@
 
-{% macro load_csv(table_name, path='', config='walmart') %}
-    {% set cfg = var('copy_into_config').get(config) %}
-    COPY INTO {{ cfg.target_schema }}.{{ table_name }}
-    FROM @{{ cfg.from_stage }}/{{ path }}
-    FILE_FORMAT = {{ cfg.file_format }}
-    FORCE={{ cfg.force }}
+{% macro load_csv(table_name, path='', key='walmart') %}
+
+    {% set c = var('copy_into_settings').get(key) %}
+
+    COPY INTO {{ c.target_schema }}.{{ table_name }}
+    FROM @{{ c.stage }}/{{ path }}
+    FILE_FORMAT = {{ c.file_format }}
+    FORCE = {{ c.force }}
+
     INCLUDE_METADATA = (loaded_at=METADATA$START_SCAN_TIME)
-    MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
-    ;
+    MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+
 {% endmacro %}
