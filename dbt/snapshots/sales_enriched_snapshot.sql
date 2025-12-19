8b43cd6bf0163eb64f09a83a71ec48631af65e95
Juno Lee <juno@junolee.com>
2025-12-19 19:49:47 +0000

    snapshot docs

--- dbt/snapshots/sales_enriched_snapshot.sql
+++ dbt/snapshots/sales_enriched_snapshot.sql
@@ -1,3 +1,15 @@
+/*
+  sales_enriched_snapshot (SCD2)
+
+  Tracks history for fct_sales_enriched using dbt snapshot (strategy: check).
+
+  Business key: (store_id, dept_id, store_date)
+  Validity: vrsn_start_date / vrsn_end_date
+  
+  New version: any check_cols change
+  Active record: vrsn_end_date is NULL
+*/
+
 {% snapshot sales_enriched_snapshot %}
 select * from {{ ref('fct_sales_enriched') }}
 {% endsnapshot %}
